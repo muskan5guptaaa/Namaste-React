d07430277fb6e9005ce7d45530fe964ad0aedbfb
muskan5guptaaa <165368032+muskan5guptaaa@users.noreply.github.com>
2024-10-01 19:29:51 +0530

    commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const parent=React.createElement("div",{id:"parent"},
+    React.createElement("div",{id:"parent"},
+    React.createElement("h1",{},"i am h1 tag")))
+
+
+
+
+
+
+const Heading=  React.createElement("h1",
+    {id:"heading",xyz:"abc"},
+    "hello world");
+
+    console.log(parent)//object
+const root= ReactDOM.createRoot( document.getElementById("root") )
+
+root.render(parent)
