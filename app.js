03b0680fa94160ce031a3c7a47b57ded50c5e10a
muskan5guptaaa <165368032+muskan5guptaaa@users.noreply.github.com>
2024-10-02 22:20:56 +0530

    commit

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
-
-
-const parent=React.createElement("div",{id:"parent"},
-    React.createElement("div",{id:"parent"},
-    React.createElement("h1",{},"i am h1 tag")))
-
-
-
-
-
-
-const Heading=  React.createElement("h1",
-    {id:"heading",xyz:"abc"},
-    "hello world");
-
-    console.log(parent)//object
-const root= ReactDOM.createRoot( document.getElementById("root") )
-
-root.render(parent)
+import React from "react";
+import ReactDOM from "react-dom/client"
+//react.createelement => object =>html element(render)
+const heading=React.createElement("h1",
+    {id:"heading"},"Namaste raect")
+//jsx is not html is it like html
+const jsxHeading=<h1 id="heading">Muskan gupta</h1>
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading)
--- dist/index.7c0ccee6.js
+++ dist/index.7c0ccee6.js
@@ -1,15 +1,3 @@
-const parent = React.createElement("div", {
-    id: "parent"
-}, React.createElement("div", {
-    id: "parent"
-}, React.createElement("h1", {}, "i am h1 tag")));
-const Heading = React.createElement("h1", {
-    id: "heading",
-    xyz: "abc"
-}, "hello world");
-console.log(parent) //object
-;
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+imp;
 
 //# sourceMappingURL=index.7c0ccee6.js.map
