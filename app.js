fecaee50d5811b002302acf1665ce1d91ee13631
muskan5guptaaa <165368032+muskan5guptaaa@users.noreply.github.com>
2024-10-02 23:10:50 +0530

    commit

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
+
+
 //react.createelement => object =>html element(render)
 const heading=React.createElement("h1",
     {id:"heading"},"Namaste raect")
+
+
 //jsx is not html is it like html
 const jsxHeading=<h1 id="heading">Muskan gupta</h1>
+
+//react component
+//class based comp-old way to writing codeeeeeeee
+
+
+//functional based comp-new way
+const HeadingComponenet=()=>{
+    <div id="container">
+  
+    <h1 className="heading">Namaste react dunc </h1>
+    </div>
+}
+
+
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading)
+root.render(<HeadingComponenet/>)
